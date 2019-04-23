9736011c5859ed97c38a973e5bac308e1dbcc508
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-23 16:27:15 +0200

    ciclo while

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-// Chiedi all’utente il cognome, 
+// Chiedi all’utente il cognome,
+var cognome = prompt ('qual è il tuo cognome?')
 // inseriscilo in un array con altri cognomi e stampa la lista
+var lista_cognomi = ['martino', 'choi', 'cattalini']
+lista_cognomi.push(cognome)
+console.log(lista_cognomi);
 // ordinata alfabeticamente.
+lista_cognomi.sort();
+console.log(lista_cognomi);
 // Scrivi a che posizione della lista il nuovo utente si trova
+var i=0;
+while (i<lista_cognomi.lenght) {
+  if (lista_cognomi[i]===cognome){console.log(i);}
+  i++;
+}
