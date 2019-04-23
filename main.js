afae07cb8c8bf88740ffab0408ee5b852786061c
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-23 15:30:21 +0200

    scrittura problema

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log('ciao');
+// Chiedi all’utente il cognome, 
+// inseriscilo in un array con altri cognomi e stampa la lista
+// ordinata alfabeticamente.
+// Scrivi a che posizione della lista il nuovo utente si trova
