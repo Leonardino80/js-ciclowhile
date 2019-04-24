ac17b8fdb711eb003fa2ac6c996681685afd7280
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-24 02:15:29 +0200

    consegna. problema dei cognomi uguali nn affrontato.

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // Chiedi all’utente il cognome,
 var cognome = prompt ('qual è il tuo cognome?')
 // inseriscilo in un array con altri cognomi e stampa la lista
-var lista_cognomi = ['martino', 'choi', 'cattalini']
-lista_cognomi.push(cognome)
-console.log(lista_cognomi);
+var cognomi = ['martino', 'choi', 'cattalini']
+cognomi.push(cognome)
+console.log(cognomi);
 // ordinata alfabeticamente.
-lista_cognomi.sort();
-console.log(lista_cognomi);
+cognomi.sort();
+console.log(cognomi);
 // Scrivi a che posizione della lista il nuovo utente si trova
 var i=0;
-while (i<lista_cognomi.lenght) {
+while (i<cognomi.lenght) {
   if (lista_cognomi[i]===cognome){console.log(i);}
   i++;
 }
